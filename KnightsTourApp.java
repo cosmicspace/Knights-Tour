7b5f5b8a4ce4a29873e004da9da3de0de14d9f53
Jason C Armstrong <bearfootcomputing@gmail.com>
2016-03-26 23:28:06 -0600

    Debugged through Eclipse
    Ran java files through Eclipse for debugging

---
+++ KnightsTourApp.java
@@ -0,0 +1,49 @@
+// Lab 3 submitted for credit for CSCI-232
+// A java implementation of the Knight's tour
+// Authors: Jason Armstrong, Anthony Fopp, Kenneth Beartusk
+// Instructor: Dr. Qing Yang
+//
+
+import java.io.*; // for reader and other io classes
+
+public class KnightsTourApp {
+	public static void main(String[] args) throws IOException {
+		int size = 0;
+	
+		// get size of board
+		System.out.print("Enter board size (8 for 8x8 board) ");
+		size = Integer.parseInt(getString());
+		System.out.println("Enter the beginning square (1 to "+size+"):");
+		Board knightsTourBoard = new Board(size);	
+	}  // end method main()
+	
+	public static String getString() throws IOException {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	} // end method getString
+
+	public static String getText() throws IOException {
+		String outStr="", str = "";
+		while(true) {
+			str = getString();
+			if( str.equals("$") ){
+				// System.out.println(outStr); // debugging output
+				return outStr;
+			} // end if
+			outStr = outStr + str + "\n";
+		} // end while
+	} // end mtehod:getText()
+
+	public static char getChar() throws IOException {
+		String s = getString();
+		return s.charAt(0);
+	} // end method:getChar
+	
+	public static int getInt() throws IOException {
+		String s = getString();
+		return Integer.parseInt(s);
+	} // end method:getInt
+	
+} // end class KnightsTourApp
--- board.java
+++ board.java
@@ -6,46 +6,53 @@
 
 import java.util.*; // for Stack class
 
-public Board (int dimension) {
-	private boolean goodPosNotOnCurrentPath[dimension]; // reads as good position, not on current path
-	private boolean goodPosNotExhausted[dimension]; // reads as good position, not exhausted. This means there are other paths to explore from this position
+public class Board {
+	private boolean[] goodPosNotOnCurrentPath; // reads as good position, not on current path
+	private boolean[] goodPosNotExhausted; // reads as good position, not exhausted. This means there are other paths to explore from this position
+	private int cycle;
+	private int currentPosition;
+	private int size;
 	private Stack boardStack = null;
-	
-	// initialize boolean values to be "good" to use
-	for (int i = 0; i<n; i++);{
-		goodPosNotOnCurrentPath[i] = true;
-		goodPosNotExhausted[i] = true;
-	}
-		
 	private static int[][] moves8 =
-			{ {+1,+2},{+2,+1},{+2,-1},{+1,-2},{-1,-2},{-2,-1},{-2,+1},{-1,+2} };
+		{ {+1,+2},{+2,+1},{+2,-1},{+1,-2},{-1,-2},{-2,-1},{-2,+1},{-1,+2} };
 	// changed from original to follow clock pattern of numbers, for easier, more intuitive, debugging
-
 	
-	public checkPosition (int position) {
-		if (goodPosNotOnCurrentPath[position] = true && 
-			goodPosNotExhausted[i] = true) {
-				return true;
-	}
-	public int getNextPos() { // picks next possible move
-		while(cycle < 8) { 
+	public Board(int dimension) { // constructor
+		// initialize boolean values to be "good" to use
+		for (int i = 0; i<dimension; i++) {
+			goodPosNotOnCurrentPath[i] = true;
+			goodPosNotExhausted[i] = true;
+		} // end for 
+	} // end constructor
+
+	public int getNextPos() { // picks next possible
+		while(cycle < 8) { // move
 			int dx = moves8[cycle][0]; // get move in
 			int dy = moves8[cycle][1]; // (x,y) format
-			int x = (position-1)%N; // translate from j
-			int y = (position-1)/N; // to (x,y) format
+			int x = (currentPosition-1)%size; // translate from j
+			int y = (currentPosition-1)/size; // to (x,y) format
 			x = x + dx; // add move to
 			y = y + dy; // position
 			cycle++; // used this move
-			if(x>=0 && x<N && y>=0 && y<N) { // on the board?
+			if(x>=0 && x<size && y>=0 && y<size) { // on the board?
 				// yes
-				int nextPos = x + y*N + 1; // (x,y) to j
-				if(knightsTourBoard.checkPosition(nextPos)==true) { // usable position?
-					// yes					
+				int nextPos	 = x + y*size + 1; // (x,y) to j
+				if(checkPosition(nextPos)) { // usable position?
+					// yes
 					return nextPos; // found a move
-				}
+				} // end if(knightsTourBoard...
 			} // end if(x>=0...)
-		} // end while // no possible move
+		} // end while // no possible move		
 		cycle = 0; // reset move index
 		return -1; // failure
 	} // end getNextPos()
-}
+	
+	public boolean checkPosition (int position) {
+		if ((goodPosNotOnCurrentPath[position] == true) && (goodPosNotExhausted[position] == true)) {
+			return true;
+		} else {
+			return false;
+		}
+	} // end method checkPosition
+
+} // end class Board
